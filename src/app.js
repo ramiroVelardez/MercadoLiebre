3e31e876e4ffabce7f71193f74b826332d64f9c5
ramiroVelardez <ramiro.velardez01@gmail.com>
2023-01-23 19:51:32 -0300

    puerto 3000 a 3001

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../public')))
 
 // Ponemos a escuchar el servidor
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log('http://localhost:' + port);
